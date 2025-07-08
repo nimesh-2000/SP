a772f5945a67a960afced6e5eba124c8a5c15415
Nimesh Denuwana <nimeshdenuwan2000@gmail.com>
2025-07-08 11:48:27 +0530

    2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 
@@ -17,17 +18,27 @@ public class Main {
         /* Problem 2 Method Calls */
         List<String> list1 = Arrays.asList("a", "b", "c");
         List<Integer> list2 = Arrays.asList(1, 2, 3);
-
         List<Object> merged = mergeAlternately(list1, list2);
         System.out.println("Problem 2");
         System.out.println(merged);
         System.out.println("---------------------------------------------------");
 
-        /* problem 3 Method Calls */
+        /* Problem 3 Method Calls */
         List<Long> first100Fib = generateFibonacci(100);
         System.out.println("Problem 3");
-
         System.out.println(first100Fib);
+        System.out.println("---------------------------------------------------");
+
+        /* Problem 4 Method Calls */
+        List<Integer> numbers = Arrays.asList(50, 2, 1, 9);
+        String largestNumber = largestFormedNumber(numbers);
+        System.out.println("Problem 4");
+        System.out.println("Largest Formed Number: " + largestNumber);
+        System.out.println("---------------------------------------------------");
+
+        /* Problem 5 Method Calls */
+        System.out.println("Problem 5");
+        findExpressions("", 1, 0, 0);
     }
 
 
@@ -105,20 +116,77 @@ public class Main {
 
         /* Problem 4 */
 
+        public static String largestFormedNumber(List<Integer> numbers) {
+
+            List<String> strNums = new ArrayList<>();
+            for (int num : numbers) {
+                strNums.add(String.valueOf(num));
+            }
+
+            Collections.sort(strNums, (a, b) -> (b + a).compareTo(a + b));
+
+            if (strNums.get(0).equals("0")) {
+                return "0";
+            }
+
+
+            StringBuilder result = new StringBuilder();
+            for (String s : strNums) {
+                result.append(s);
+            }
+
+            return result.toString();
+        }
+
+
+        /* Problem 5 *
 
 
 
+        /**
+         * Recursive function to explore all combinations
+         *
+         * expression -> Current expression string
+         * num -> Current number being considered (1 to 9)
+         * sum -> Current total sum of expression
+         * lastValue -> Last value added/subtracted (used for concatenation)
+         */
+        public static void findExpressions(String expression, int num, long sum, long lastValue) {
+            if (num > 9) {
+                if (sum == 100) {
+                    System.out.println(expression);
+                }
+                return;
+            }
 
+            String currentStr = String.valueOf(num);
 
+            if (expression.isEmpty()) {
+                // First digit
+                findExpressions(currentStr, num + 1, num, num);
+            } else {
+                // Add '+'
+                findExpressions(expression + "+" + currentStr, num + 1, sum + num, num);
 
+                // Add '-'
+                findExpressions(expression + "-" + currentStr, num + 1, sum - num, -num);
 
+                // Concatenate (no operator)
+                long newLast = concatenate(lastValue, num);
+                long newSum = sum - lastValue + newLast;
+                findExpressions(expression + currentStr, num + 1, newSum, newLast);
+            }
+        }
 
-
-    public static void problem_2(){
-
+        // Concatenate two numbers, preserving the sign of the first
+        private static long concatenate(long base, int next) {
+            boolean negative = base < 0;
+            long abs = Math.abs(base);
+            long result = abs * 10 + next;
+            return negative ? -result : result;
+        }
     }
-    public static void problem_3(){
 
-    }
-}
+
+
 
